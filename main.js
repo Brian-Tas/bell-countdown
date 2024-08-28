d1041107edf9f3d98585249bec3d1dc45cf6f1d4
Brian-Tas <brianjtascone@gmail.com>
2024-08-28 01:56:37 +0000

    created a time counter and a loop

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let periodTable;
 
-const toMinutes = time => {
-    return((time[0] * 60) + time[1]);
+const toSeconds = time => {
+    return((time[0] * 3600) + time[1] * 60 + (time[2] ? time[2] : 0) + (time[3] ? time[3]/10 : 0));
 }
 
 const toTime = minutes => {
@@ -14,11 +14,31 @@ const getJson = async jsonLink => {
     periodTable = refined;
 }
 
-const unpackageJson = json => {
-    
+const jsonToSeconds = data => {
+    let dataArr = data.split(':');
+
+    for(let i = 0; i < dataArr.length; i++) {
+        dataArr[i] = parseInt(dataArr[i]);
+    }
+
+    return toSeconds(dataArr);
 }
 
 (async () => {
     await getJson('./times.json');
-    console.log(periodTable);
+    console.log(jsonToSeconds(periodTable.normal.hr));
+
+    setInterval(()=>{
+        const currentTime = new Date();
+    
+        const currentMinutes = toSeconds([
+        currentTime.getHours(),
+        currentTime.getMinutes(),
+        currentTime.getSeconds(),
+        Math.round(currentTime.getMilliseconds()/100) //deciseconds or tenths of seconds
+        ]);
+    
+        
+        console.log(currentMinutes);
+    },100)
 })();

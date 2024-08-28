cb6e8576d0b611329152feaaa6077068dff0ff21
Brian-Tas <brianjtascone@gmail.com>
2024-08-28 18:34:08 +0000

    Made a number that counts down to end period

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 let periodTable;
-
 const toSeconds = time => {
-    return((time[0] * 3600) + time[1] * 60 + (time[2] ? time[2] : 0) + (time[3] ? time[3]/10 : 0));
+    return((time[0] * 3600) + (time[1] ? time[1] * 60 : 0) + (time[2] ? time[2] : 0) + (time[3] ? time[3]/10 : 0));
 }
-
 const toTime = minutes => {
     return([Math.floor(time/60), time % 60]);
 }
-
 const getJson = async jsonLink => {
     const raw = await fetch(jsonLink);
     const refined = await raw.json();
     periodTable = refined;
 }
-
 const jsonToSeconds = data => {
     let dataArr = data.split(':');
 
@@ -23,22 +19,29 @@ const jsonToSeconds = data => {
 
     return toSeconds(dataArr);
 }
-
+const getPeriod = inputTime => {
+    const currentDate = new Date();
+    const dayTable = eval("periodTable." + (currentDate.getDay() === '3' || currentDate.getDay() === '4' ? 'normal' : 'jaguar'))
+    const timesTable = Object.values(dayTable); 
+    
+    for(let i = 0; i < timesTable.length; i++) {
+        if(inputTime < jsonToSeconds(timesTable[i])) {
+            return jsonToSeconds(Object.values(dayTable)[i]);
+        }
+    }
+}
 (async () => {
     await getJson('./times.json');
-    console.log(jsonToSeconds(periodTable.normal.hr));
-
+    
     setInterval(()=>{
-        const currentTime = new Date();
-    
-        const currentMinutes = toSeconds([
-        currentTime.getHours(),
-        currentTime.getMinutes(),
-        currentTime.getSeconds(),
-        Math.round(currentTime.getMilliseconds()/100) //deciseconds or tenths of seconds
+        const currentDate = new Date();
+        const currentSeconds = toSeconds([
+            currentDate.getHours(),
+            currentDate.getMinutes(),
+            currentDate.getSeconds(),
+            Math.round(currentDate.getMilliseconds()/100) //deciseconds or tenths of seconds
         ]);
-    
         
-        console.log(currentMinutes);
+        document.getElementById("title-text").innerHTML = (getPeriod(currentSeconds) - currentSeconds).toFixed(1);
     },100)
 })();

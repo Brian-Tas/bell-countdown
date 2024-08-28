f0be3fecbaea4fd43cc378a58a86ad08ca5a4996
Brian-Tas <brianjtascone@gmail.com>
2024-08-28 22:32:47 +0000

    Replicated Original bell countdown

--- main.js
+++ main.js
@@ -2,33 +2,28 @@ let periodTable;
 const toSeconds = time => {
     return((time[0] * 3600) + (time[1] ? time[1] * 60 : 0) + (time[2] ? time[2] : 0) + (time[3] ? time[3]/10 : 0));
 }
-const toTime = minutes => {
-    return([Math.floor(time/60), time % 60]);
-}
 const getJson = async jsonLink => {
     const raw = await fetch(jsonLink);
     const refined = await raw.json();
     periodTable = refined;
 }
-const jsonToSeconds = data => {
+const templateToSeconds = data => {
     let dataArr = data.split(':');
-
-    for(let i = 0; i < dataArr.length; i++) {
+    for(let i = 0; i < data.length; i++) {
         dataArr[i] = parseInt(dataArr[i]);
     }
-
     return toSeconds(dataArr);
 }
 const getPeriod = inputTime => {
     const currentDate = new Date();
     const dayTable = eval("periodTable." + (currentDate.getDay() === '3' || currentDate.getDay() === '4' ? 'normal' : 'jaguar'))
     const timesTable = Object.values(dayTable); 
-    
     for(let i = 0; i < timesTable.length; i++) {
-        if(inputTime < jsonToSeconds(timesTable[i])) {
-            return jsonToSeconds(Object.values(dayTable)[i]);
+        if(inputTime < templateToSeconds(timesTable[i])) {
+            return templateToSeconds(Object.values(dayTable)[i]);
         }
-    }
+    } 
+    return templateToSeconds(periodTable.normal.hr) + 86400; //hr seconds + seconds in a day
 }
 (async () => {
     await getJson('./times.json');
@@ -38,10 +33,12 @@ const getPeriod = inputTime => {
         const currentSeconds = toSeconds([
             currentDate.getHours(),
             currentDate.getMinutes(),
-            currentDate.getSeconds(),
-            Math.round(currentDate.getMilliseconds()/100) //deciseconds or tenths of seconds
+            currentDate.getSeconds()
         ]);
-        
-        document.getElementById("title-text").innerHTML = (getPeriod(currentSeconds) - currentSeconds).toFixed(1);
-    },100)
+        let timeDifference = getPeriod(currentSeconds) - currentSeconds;
+
+        document.getElementById("hours").innerHTML = Math.floor(timeDifference / 3600) + ' Hours';
+        document.getElementById("minutes").innerHTML = Math.floor((timeDifference % 3600) / 60) + " Minutes"
+        document.getElementById("seconds").innerHTML = Math.floor((timeDifference % 3600) % 60) + " Seconds"
+    },10)
 })();
